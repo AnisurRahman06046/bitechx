f00b119e345513328f7ef222539cc3c9c27d3a62
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-03-15 21:22:21 +0600

    all routes for user are done

--- src/app/modules/User/User.route.ts
+++ src/app/modules/User/User.route.ts
@@ -3,4 +3,8 @@ import { userControllers } from './User.controller';
 const router = Router();
 
 router.post('/create', userControllers.createUser);
+router.get('/all-users', userControllers.users);
+router.get('/user/:id', userControllers.user);
+router.patch('/user/:id', userControllers.updateUser);
+router.delete('/user/:id', userControllers.deleteUser);
 export const userRoutes = router;
