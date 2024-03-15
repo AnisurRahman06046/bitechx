629694473fcda7d9a2b4db1a7710d8114aba0e2c
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-03-15 22:02:32 +0600

    done

--- src/app/modules/User/User.service.ts
+++ src/app/modules/User/User.service.ts
@@ -1,7 +1,11 @@
+import { checkUser } from '../../utils/checkUser';
 import { IUser } from './User.interface';
 import { User } from './User.model';
 
 const createUser = async (payload: IUser) => {
+  // check if user exists
+  const isUserExist = await User.findOne({ email: payload.email });
+  if (isUserExist) throw new Error('User already exist');
   const result = await User.create(payload);
   return result;
 };
@@ -14,17 +18,26 @@ const users = async () => {
 
 // fetch single user
 const user = async (id: string) => {
-  const result = await User.findOne({ id });
+  if (!(await checkUser(id))) {
+    throw new Error('User not found');
+  }
+  const result = await User.findById(id);
   return result;
 };
 // update a user
 const editUser = async (id: string, payload: Partial<IUser>) => {
+  if (!(await checkUser(id))) {
+    throw new Error('User not found');
+  }
   const result = await User.findByIdAndUpdate(id, payload, { new: true });
   return result;
 };
 
 // delete a user
 const deleteUser = async (id: string) => {
+  if (!(await checkUser(id))) {
+    throw new Error('User not found');
+  }
   const result = await User.findByIdAndDelete(id);
   return result;
 };
